26d2ab02ef31a9faa8ad8cd7b53690b0e0a70b0d
JeongsolKim <60421110+JeongsolKim@users.noreply.github.com>
2020-11-17 16:48:06 +0900

    add query_to_modified_line_graphs to preprocess.py

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleFFN(nn.Module):
+    def __init__(self, in_feats, nhid, num_classes, activation='relu'):
+        super(SimpleFFN, self).__init__()
+        self.dense1 = nn.Linear(in_feats, nhid, bias=True)
+        self.dense2 = nn.Linear(nhid, nhid, bias=True)
+        self.dense3 = nn.Linear(nhid, num_classes, bias=True)
+
+    def forward(self, x):
+        x = F.relu(self.dense1(x))
+        x = F.relu(self.dense2(x))
+        x = F.log_softmax(self.dense3(x))
+
+        return x
+
+
--- train.py
+++ train.py
@@ -1,11 +1,24 @@
 import dgl
+from dgl.data.utils import load_graphs
 import torch
-from utils import *
 import os
 import warnings
+from preprocess import *
+from model import *
 
 warnings.filterwarnings('ignore')
 
-preprocess(data='train')
-preprocess(data='valid')
-preprocess(data='test')
+glist, label_dict = load_graphs("./processed/train_graphs_1.bin", [0])
+g = glist[0]
+
+model = SimpleFFN(in_feats=1800, nhid=512, num_classes=25)
+
+print(model(g))
+# DOTO
+# 1. make customized dataset class
+# 2. make function for constructing the modified line graphs
+# 3. make entire structure for training
+# - for SVM
+# - for simple FFN
+# - for GraphSAGE
+# 4. graphics.
