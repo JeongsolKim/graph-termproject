6df3c890bcf4188067ca3ba97b14423cd849e81e
JeongsolKim <60421110+JeongsolKim@users.noreply.github.com>
2020-11-18 22:33:21 +0900

    add proposed model based on GraphSAGE

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import dgl
+import dgl.nn.pytorch as dglnn
 
 class SimpleFFN(nn.Module):
     def __init__(self, in_feats, nhid, num_classes, activation='relu'):
@@ -19,4 +20,52 @@ class SimpleFFN(nn.Module):
 
         return x
 
+
+class MyModel(nn.Module):
+    def __init__(self, in_dim, hidden_dim, num_classes, aggregator='mean', activation='sigmoid'):
+        super(MyModel, self).__init__()
+        # We make two GNN model for H1 and H2.
+        self.SAGE1 = GraphSage(in_dim, hidden_dim, num_classes, aggregator, activation)
+        self.SAGE2 = GraphSage(in_dim, hidden_dim, num_classes, aggregator, activation)
+
+    def forward(self, H1, H2, feats):
+        H1_rep = self.SAGE1(H1, feats)
+        H2_rep = self.SAGE2(H2, feats)
+
+        # last aggregation
+        return (H1_rep+H2_rep)/2
+
+
+class GraphSage(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim, aggregator='mean', activation='sigmoid'):
+        super(GraphSage, self).__init__()
+        self.sageconv1 = dglnn.SAGEConv(in_feats=in_dim,
+                                        out_feats=hidden_dim,
+                                        aggregator_type=aggregator,
+                                        bias=False)
+        self.sageconv2 = dglnn.SAGEConv(in_feats=hidden_dim,
+                                        out_feats=hidden_dim,
+                                        aggregator_type=aggregator,
+                                        bias=False)
+        self.linear = nn.Linear(hidden_dim, out_dim, bias=False)
+        self.activation = activation
+
+    def forward(self, g, feats):
+        feats = F.relu(self.sageconv1(g, feats))
+        feats = F.relu(self.sageconv2(g, feats))
+        with g.local_scope():
+            g.ndata['feats'] = feats
+            # Read-out
+            graph_rep = dgl.mean_nodes(g, 'feats')
+            # Linear
+            graph_rep = self.linear(graph_rep)
+
+            if self.activation == 'relu':
+                return F.relu(graph_rep)
+            elif self.activation == 'sigmoid':
+                return torch.sigmoid(graph_rep)
+            elif self.activation == 'softmax':
+                return F.softmax(graph_rep)
+            else:
+                return graph_rep
 
--- test.py
+++ test.py
@@ -1,22 +1,15 @@
 from preprocess import *
 from utils import *
+from model import *
 import time
 import numpy as np
 import torch
 
-out = np.zeros((1,5))
-out[0,3] = 1
-out[0,4] = 1
-label = np.zeros((1,5))
-label[0,1] = 1
-label[0,3] = 1
+H1, H2, feats, label = load_graph('./train/train_000.txt')
 
+model = MyModel(in_dim=feats.size()[-1],
+                hidden_dim=64,
+                num_classes=label.size()[-1])
 
-out = torch.tensor(out)
-label = torch.tensor(label)
-loss = torch.nn.BCELoss()
-
-f1 = f1_score(out, label)
-print(f1)
-
-_,_,_,_ = load_graph('./valid_query/valid_query_000.txt')
+output = model(H1, H2, feats)
+print(output)
